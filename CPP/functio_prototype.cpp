84f11d3d7559e012cbab4d2169b604e223eb28a9
shailendra parihar <113036851+shelu18@users.noreply.github.com>
2022-10-08 17:50:19 +0530

    Add files via upload

---
+++ CPP/functio_prototype.cpp
@@ -0,0 +1,17 @@
+//functions
+#include<iostream>
+using namespace std;
+int add (int num1,int num2){
+
+int sum=num1+num2;
+
+
+}
+int main(){
+
+
+    int a=2;
+    int b=3;
+    cout<<"the sum is "<<add(a,b)<<endl;
+    return 0;
+}
