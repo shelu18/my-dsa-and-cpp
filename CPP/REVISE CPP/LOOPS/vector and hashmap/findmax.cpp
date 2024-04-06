8e09ddf6ae1ae5d269cc644155c209e389b71a0d
Shailendra Parihar <113036851+shelu18@users.noreply.github.com>
2024-04-06 09:20:42 +0530

    Add files via upload

---
+++ CPP/REVISE CPP/LOOPS/vector and hashmap/findmax.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+int max = nums[0];
+    int maxElement(vector<int>& nums) {
+        for(int i=0;i<nums.size()i++){
+            if(nums[i]>max){
+                max =num[i]
+            }
+            
+        }
+    
+    }
+    return max;
+};
